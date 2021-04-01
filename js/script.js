3266529d1a3cfa8567101e4286d1d21671e5c591
Valci Júnior <valcijunior19@hotmail.com>
2021-04-01 18:47:21 -0300

    criacao dos obstatulos e mudanca na funcao game over

--- js/script.js
+++ js/script.js
@@ -1,11 +1,21 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
-
 let box = 32;
+
 let img = new Image(box, box);
 let img_jerry = new Image(box, box);
-img.src = 'snake.jpg'
-img_jerry.src = 'jerry.png'
+let img_obst1 = new Image(box, box);
+
+img.src = 'imgs/snake.jpg'
+img_jerry.src = 'imgs/jerry.png'
+img_obst1.src = 'imgs/pedra.jpg';
+
+let drawPrint = false;
+let level = 200;
+let pointes = 0;
+let cont = 0;
+let lives = 10;
+
 let snake = [];
 snake[0] = {
     x: 8 * box,
@@ -15,7 +25,13 @@ snake[0] = {
 let food = {
     x: Math.floor(Math.random() * 15 + 1) * box,
     y: Math.floor(Math.random() * 15 + 1) * box
+} 
+
+let obstacle = {
+    x: food.x - box,
+    y: food.y
 }
+
 
 let direction = "right";
 
@@ -36,6 +52,12 @@ function drawFood(){
     let pattern2 = context.createPattern(img_jerry, 'repeat')
     context.fillStyle = pattern2;
     context.fillRect(food.x, food.y, box, box);
+}
+
+function drawObst(){
+    let pattern3 = context.createPattern(img_obst1, 'repeat');
+    context.fillStyle = pattern3;
+    context.fillRect(obstacle.x, obstacle.y, box, box);
 }
 
 document.addEventListener("keydown", update);
@@ -61,16 +83,26 @@ function update(event){
 
 
 function playGame(){
-
+    
     if (snake[0].x > 15 * box && direction === "right"){ snake[0].x = 0;}
     if (snake[0].x < 0 && direction === "left"){ snake[0].x = 16 * box;}
     if (snake[0].y > 15 * box && direction === "down"){ snake[0].y = 0;}
     if (snake[0].y < 0 && direction === "up"){ snake[0].y = 16 * box;}
     
+   
     for(i = 1; i < snake.length; i++){
+
         if (snake[0].x === snake[i].x && snake[0].y === snake[i].y){
-            clearInterval(game);
-            alert("Game Over :(.\nReinicie a página para jogar novamente");
+            gameOver();
+            
+        }else if (snake[0].x === obstacle.x && snake[0].y === obstacle.y && drawPrint === true){
+            snake.pop();
+            lives -= 1;
+            console.log(lives)
+            if (lives < 0){
+                gameOver();
+                console.log('entrei no if')
+            }
         }
     }
 
@@ -78,6 +110,12 @@ function playGame(){
     img.onload();
     drawFood();
     
+    if (cont > 3) {
+        drawPrint = true;
+        drawObst();
+    }
+   
+
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
 
@@ -90,8 +128,23 @@ function playGame(){
         snake.pop();
     }else{
 
+        cont += 1;
+       
+        pointes += 10;
+
+        if(cont == 5){
+            pointes += 15;
+            level = 150;
+        }
+        else if (cont > 12){
+            pointes += 30;
+            level = 200;
+        }
+
         food.x = Math.floor(Math.random() * 15 + 1) * box;
         food.y = Math.floor(Math.random() * 15 + 1) * box;
+        obstacle.x = food.x - box;
+        obstacle.y = food.y;
     }
     
 
@@ -103,4 +156,10 @@ function playGame(){
     snake.unshift(newHead);
 }
 
-let game = setInterval(playGame, 110);
+function gameOver(){
+
+    clearInterval(game);
+    alert("Game Over :(.\nReinicie a página para jogar novamente");
+}
+
+let game = setInterval(playGame, level);
